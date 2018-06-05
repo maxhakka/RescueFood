69d36d50a3f39d11caf4de5f718d29e94040b6ea
maxhakka <hakka@kth.se>
2018-06-05 16:30:01 +0200

    Added pages

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,14 +1,127 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'page-contact',
   templateUrl: 'contact.html'
 })
 export class ContactPage {
+  @ViewChild('barCanvas') barCanvas;
+  @ViewChild('moneyCanvas') moneyCanvas;
+  @ViewChild('carbonCanvas') carbonCanvas;
+ 
+  barChart: any;
+  moneyChart: any;
+  carbonChart: any;
 
   constructor(public navCtrl: NavController) {
 
   }
+  ionViewDidLoad() {
+ 
+    this.barChart = new Chart(this.barCanvas.nativeElement, {
+
+        type: 'horizontalBar',
+        data: {
+            labels: ["My Portions", "Average"],
+            datasets: [{
+                label: '# of Portions',
+                data: [13, 10],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            legend: {
+                display: false
+            },
+            scales: {
+                xAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }]
+            }
+        }
+
+    });
+
+    this.moneyChart = new Chart(this.moneyCanvas.nativeElement, {
+
+      type: 'horizontalBar',
+      data: {
+          labels: ["My Savings", "Average"],
+          datasets: [{
+              label: 'Amount of SEK',
+              data: [534, 320],
+              backgroundColor: [
+                  'rgba(255, 99, 132, 0.2)',
+                  'rgba(54, 162, 235, 0.2)'
+              ],
+              borderColor: [
+                  'rgba(255,99,132,1)',
+                  'rgba(54, 162, 235, 1)'
+              ],
+              borderWidth: 1
+          }]
+      },
+      options: {
+          legend: {
+              display: false
+          },
+          scales: {
+              xAxes: [{
+                  ticks: {
+                      beginAtZero:true
+                  }
+              }]
+          }
+      }
+
+    });
+
+    this.carbonChart = new Chart(this.carbonCanvas.nativeElement, {
+
+      type: 'horizontalBar',
+      data: {
+          labels: ["My Contribution", "Average"],
+          datasets: [{
+              label: 'Amount of CO2',
+              data: [34, 25],
+              backgroundColor: [
+                  'rgba(255, 99, 132, 0.2)',
+                  'rgba(54, 162, 235, 0.2)'
+              ],
+              borderColor: [
+                  'rgba(255,99,132,1)',
+                  'rgba(54, 162, 235, 1)'
+              ],
+              borderWidth: 1
+          }]
+      },
+      options: {
+          legend: {
+            display: false
+          },
+          scales: {
+              xAxes: [{
+                  ticks: {
+                      beginAtZero:true
+                  }
+              }]
+          }
+      }
+    });
+
+}
+  
 
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,42 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { RestaurantPage } from '../restaurant/restaurant';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  visible: boolean;
+  visible2: boolean;
+  visible3: boolean;
+  visible4: boolean;
 
   constructor(public navCtrl: NavController) {
+    this.visible = false;
+    this.visible2 = false;
+    this.visible3 = false;
+    this.visible4 = false;
+  }
 
+  nextPage(){
+    this.navCtrl.push(RestaurantPage);
+  }
+
+  toggle(restaurang: number) {
+    if (restaurang == 1) {
+      this.visible = !this.visible;
+    }
+    if (restaurang == 2) {
+      this.visible2 = !this.visible2;
+    }
+    if (restaurang == 3) {
+      this.visible3 = !this.visible3;
+    }
+    if (restaurang == 4) {
+      this.visible4 = !this.visible4;
+    }
+   
   }
 
 }
